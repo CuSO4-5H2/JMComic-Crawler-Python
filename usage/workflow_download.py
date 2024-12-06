077e8dde552187cd3341f9665f34b39018a67198
CuSO4-5H2 <150568928+CuSO4-5H2@users.noreply.github.com>
2024-12-07 03:07:15 +0800

    Update workflow_download.py

--- usage/workflow_download.py
+++ usage/workflow_download.py
@@ -3,7 +3,8 @@ from jmcomic.cl import JmcomicUI
 
 # 下方填入你要下载的本子的id，一行一个，每行的首尾可以有空白字符
 jm_albums = '''
-https://18comic.vip/album/151280
+https://18comic.vip/album/1029515
+https://18comic.vip/album/1029088
 '''
 
 # 单独下载章节
